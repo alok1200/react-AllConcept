6a70393f68b8c09cc86c579fbc33b17ad531f672
Alok Singh <AlokSingh1200@gmail.com>
2025-08-08 14:01:22 +0530

    solve all problem

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -1,20 +1,20 @@
-// there are creating a element in the dom (element) and adding the properties to it
 function customRender(reactElement, container) {
-  const domElement = document.createElement(reactElement.type); // creating that element in dom
-  domElement.innerHtml = reactElement.children; // adding the children to the element
-  domElement.setAttribute("href", reactElement.prop.href); // creating the attribute
-  domElement.setAttribute("tergatt", reactElement.prop.tergate); // creating the attribute
+  const domElement = document.createElement(reactElement.type);
+  domElement.textContent = reactElement.children;
+  domElement.setAttribute("href", reactElement.prop.href);
+  domElement.setAttribute("target", reactElement.prop.target);
 
-  container.appendChild(domeElement); // adding the element to the container
+  container.appendChild(domElement);
 }
 
 const reactElement = {
   type: "a",
   prop: {
-    href: "https://www.google.com",
+    href: "https://www.google.com/",
     target: "_blank",
   },
   children: "click me to visit google",
 };
 
 const mainContainer = document.querySelector("#root");
+customRender(reactElement, mainContainer);
