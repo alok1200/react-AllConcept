86924ef9a71f1e9dac70a3abdd8696f3df96f2db
Alok Singh <AlokSingh1200@gmail.com>
2025-08-10 14:06:33 +0530

    creating a add and sub value using a state

--- 1react/src/App.jsx
+++ 1react/src/App.jsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
+
 function App() {
+  const [count, setCount] = useState(0);
+
+  const handleAdding = () => setCount(count + 1);
+  const handleSubtracting = () => setCount(count - 1);
+
   return (
     <>
-      <h1> hello theree</h1>
+      <h1>Count: {count}</h1>
+      <button onClick={handleAdding}>Add</button>
+      <button onClick={handleSubtracting}>Subtract</button>
     </>
   );
 }
