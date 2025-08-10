b84c2077e24dd1cbe249cc8dd5e9cfeef7296b06
Alok Singh <AlokSingh1200@gmail.com>
2025-08-10 21:06:30 +0530

    todya i learn tailwind and using some card for adding

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import Cart from "./component/Cart";
+
+function App(props) {
+  let test = {
+    name: "1test",
+    age: "18",
+  };
+
+  return (
+    <>
+      <h1 className="bg-green-500 text-color-white"> this is for test</h1>
+      <Cart test="test" testObject={test} />
+    </>
+  );
+}
+
+export default App;
